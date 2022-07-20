9c05a1eaad835d7df5a56802fa80285b38931fd9
a-lukasiewicz <adamlukasiewicz3@gmail.com>
2022-07-20 15:06:41 +0200

    auth home fixed

---
+++ src/components/authorized/home/Technology.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex items-center justify-start w-full px-4 py-2 rounded md:w-1/2 lg:w-1/3 gray-300 y-2"
+  >
+    <img class="w-8 h-8 mr-3" :src="imageURL" alt="Tech" />
+    <h3 class="font-bold">{{ language }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  language: { type: String, default: '' },
+  img: { type: String, default: '' },
+});
+const imageURL = new URL(`/src/assets/tech/${props.img}`, import.meta.url).href;
+</script>
